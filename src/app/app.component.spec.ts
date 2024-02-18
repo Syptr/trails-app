20cdee4b295f62e770f6be6aca66881cf48b5984
syptr <scj.cr8s@gmail.com>
2024-02-18 11:37:36 -0500

    'Adds img, footer, and header to main page'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,10 +2,14 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+ 
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+  
   });
 
   it('should create the app', () => {
@@ -17,13 +21,30 @@ describe('AppComponent', () => {
   it(`should have the 'trails-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('trails-app');
+    expect(app.title).toEqual('NH Trails');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, trails-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('NH Trails');
   });
+
+  it('should render an image', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    // expect(compiled.querySelector('img')?.tagName).toBe('id', 'greely-pond');
+    expect(compiled.querySelector('img')).not.toBeNull();
+    expect(compiled.querySelector('img')?.getAttribute('src')).toBe('/assets/greely-pond.jpg');
+  });
+
+  it('should render a footer', () =>{
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('footer')?.textContent).toContain('NH Trails');
+  })
 });
